e78ca28e11a0e3e0c6abdba4ff7e34606ce29237
3ssalunke <surajsalunke1276@gmail.com>
2025-03-23 00:53:12 +0530

    220325: [design pattern(c#)] creational - prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,14 @@
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Badru");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+var employee = new Employee("Dhaval", managerClone);
+var employeeClone = (Employee)employee.Clone(true);
+Console.WriteLine($"Employee was cloned: {employeeClone.Name} {employeeClone.Manager.Name}");
+
+managerClone.Name = "Paul";
+Console.WriteLine($"Employee was cloned: {employeeClone.Name} {employeeClone.Manager.Name}");
