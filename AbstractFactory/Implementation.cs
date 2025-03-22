c4e6df6b301baf18bb1195e981229c1036a37594
3ssalunke <surajsalunke1276@gmail.com>
2025-03-22 23:26:34 +0530

    220325: [design pattern(c#)] creational - abstract factory

---
+++ AbstractFactory/Implementation.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public interface IShoppingCartPurchaseFactory
+    {
+        IDiscountService CreateDiscountService();
+        IShippingCostService CreateShippingCostService();
+    }
+
+    public interface IDiscountService
+    {
+        public int DiscountPercentage { get; }
+    }
+
+    public interface IShippingCostService
+    {
+        public decimal ShippingCosts { get; }
+    }
+
+    public class IndiaDiscountService : IDiscountService
+    {
+        public int DiscountPercentage => 20;
+    }
+
+    public class AustraliaDiscountService : IDiscountService
+    {
+        public int DiscountPercentage => 10;
+    }
+
+    public class IndiaShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCosts => 5;
+    }
+
+    public class AustraliaShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCosts => 4;
+    }
+
+    public class IndiaShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new IndiaDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new IndiaShippingCostService();
+        }
+    }
+
+    public class AustraliaShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new AustraliaDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new AustraliaShippingCostService();
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IShippingCostService _shippingCostService;
+        private int _orderCosts;
+
+        public ShoppingCart(IShoppingCartPurchaseFactory factory)
+        {
+            _discountService = factory.CreateDiscountService();
+            _shippingCostService = factory.CreateShippingCostService();
+            _orderCosts = 200;
+        }
+
+        public void CalculateCosts()
+        {
+            Console.WriteLine($"Total Costs = {_orderCosts - (_orderCosts / 100 * _discountService.DiscountPercentage) + _shippingCostService.ShippingCosts}");
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,11 @@
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+var indiaShoppingCartPurchaseFactory = new IndiaShoppingCartPurchaseFactory();
+var shoppingCartForIndia = new ShoppingCart(indiaShoppingCartPurchaseFactory);
+shoppingCartForIndia.CalculateCosts();
+
+var australiaShoppingCartPurchaseFactory = new AustraliaShoppingCartPurchaseFactory();
+var shoppingCartForAustralia = new ShoppingCart(australiaShoppingCartPurchaseFactory);
+shoppingCartForAustralia.CalculateCosts();
