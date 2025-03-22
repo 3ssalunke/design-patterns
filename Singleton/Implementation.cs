b273592d20f78fd25b8d80f32bb6be15b8106ad2
3ssalunke <surajsalunke1276@gmail.com>
2025-03-22 20:18:29 +0530

    220325: [design pattern(c#)] creational - singleton

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,29 @@
+namespace Singleton
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+        // private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                // if (_instance == null)
+                // {
+                //     _instance = new Logger();
+                // }
+                // return _instance;
+                return _lazyLogger.Value;
+            }
+        }
+
+        protected Logger()
+        { }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
