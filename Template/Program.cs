0017fdfa1b53914db06a3a02aa6ca45f8b8a2814
3ssalunke <surajsalunke1276@gmail.com>
2025-03-23 23:35:25 +0530

    220325: [design pattern(c#)] behavioural - template

---
+++ Template/Program.cs
@@ -0,0 +1,15 @@
+﻿using Template;
+
+Console.Title = "Template";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("abcd1234"));
+Console.WriteLine();
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("1234abcd"));
+Console.WriteLine();
+
+EudoraMailParser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody("qwer9876"));
+Console.WriteLine();
