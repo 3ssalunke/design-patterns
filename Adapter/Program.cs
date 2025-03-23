593c08f43447d17914fed63b3fc3dfc0384b31b1
3ssalunke <surajsalunke1276@gmail.com>
2025-03-23 13:33:10 +0530

    220325: [design pattern(c#)] structural - adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,8 @@
+﻿using ClassAdapter;
+
+Console.Title = "Adapter";
+
+ICityAdapter Adapter = new CityAdapter();
+var city = Adapter.GetCity();
+
+Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
